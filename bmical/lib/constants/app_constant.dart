a4ee8aab84d3b0ff9985a0ee0fc23fddf35a1a47
sumudu.2018348@iit.ac.lk <sumudu.2018348@iit.ac.lk>
2023-06-13 21:44:28 +0530

    2nd commit

---
+++ bmical/lib/constants/app_constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#17202A");
+final Color accentHexColor = HexColor('#10ff00');
